edce58eb1d893ab769e946299d7ba87b628ea911
Michael Costaños <michael.costanos11@gmail.com>
2023-04-19 06:31:34 +0800

    next is Style components with CSS Modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
-import * as react from "react";
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>Home Page</title>;
+
+export default IndexPage;
